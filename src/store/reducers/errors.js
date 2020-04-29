f889c7570912e601f1c85f63001231477b294184
Michael <michaelpp2001@gmail.com>
2020-04-29 16:38:15 -0700

    brought our store into our app.js, and imported our router

--- src/store/reducers/errors.js
+++ src/store/reducers/errors.js
@@ -1,7 +1,7 @@
 import {ADD_ERROR, REMOVE_ERROR} from "../actionTypes";
 
 // generic error handler reducer for all types of errors
-export default (state = {message: bull}, action) => {
+export default (state = {message: null}, action) => {
     switch (action.type) {
         case ADD_ERROR:
             return {...state, message: action.error};
