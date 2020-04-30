8ccbc232347c581283b9cec6fb364fb132882b62
Michael <michaelpp2001@gmail.com>
2020-04-30 14:39:12 -0700

    added a Main component that renders a hompage route

---
+++ src/containers/Main.js
@@ -0,0 +1,25 @@
+// Main will be responsible for our routing logic
+
+ import React from "react";
+ import {Switch, Route, withRouter, Redirect} from "react-router-dom";
+ import {connect} from "react-redux";
+ import Homepage from "../components/Homepage";
+
+ // Switch component will allow for multiple routes and 404 logic
+ const Main = props => {
+     return (
+        <div className="container">
+            <Switch>
+                <Route exact path="/" render={props => <Homepage {...props} />} />
+            </Switch>
+        </div>
+    );
+ };
+
+ function mapStateToProps(state) {
+     return {
+         currentUser: state.currentUser
+     };
+ }
+
+ export default withRouter(connect(mapStateToProps, null))(Main);
