d1ede036b0a38d7af2c8ed3d54ced091fbee4c64
Michael <michaelpp2001@gmail.com>
2020-04-30 16:41:47 -0700

    added some comments to our apiCall method

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios"
+import errors from "../store/reducers/errors"
+import { useCallback } from "react"
 
 // create our own promise(since apiCall will be asynchronous)
 // passes in a method to use on axios (hence no dot notation)
+
+// A wrapper around axios API call that formats errors, etc 
+// @param {string} method the HTTP verb you want to use
+// @param {string} path the route path/endpoint
+// @param {object} data {optional} data in JSON form for POST
+
 export function apiCall(method, path, data) {
     return new Promise((resolve, reject) => {
         return axios[method](path, data)
