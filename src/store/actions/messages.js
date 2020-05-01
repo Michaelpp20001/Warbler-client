b2f24ea9d502a2af0aef3f5576e04dce4fa11ac4
Michael <michaelpp2001@gmail.com>
2020-05-01 13:25:10 -0700

    delete messages

--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -1,12 +1,29 @@
 import {apiCall} from "../../services/api";
 import {addError} from "./errors";
 import {LOAD_MESSAGES} from "../actionTypes";
-import {REMOVE_MESSAGES} from "../actionTypes";
+import {REMOVE_MESSAGE} from "../actionTypes";
 
+
+// action creators
 export const loadMessages = messages => ({
     type: LOAD_MESSAGES,
     messages
 });
+
+export const remove = id => ({
+    type: REMOVE_MESSAGE,
+    id
+})
+
+// thunks
+export const removeMessage = (user_id, message_id) => {
+    return dispatch => {
+        return apiCall("delete", `/api/users/${user_id}/messages/${message_id}`)
+        .then(() => dispatch(remove(message_id)))
+        .catch(err => addError(err.message));
+    };
+};
+
 
 export const fetchMessages = () => {
     return async dispatch => {
